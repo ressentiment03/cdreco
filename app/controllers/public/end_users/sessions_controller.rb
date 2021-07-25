d7bae9540cfb17138e4eabee450e348dbf4ab833
EC2 Default User <ec2-user@ip-172-31-5-148.us-east-2.compute.internal>
2021-07-25 12:51:45 +0000

    add account_manage

---
+++ app/controllers/public/end_users/sessions_controller.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+class Public::EndUsers::SessionsController < Devise::SessionsController
+  # before_action :configure_sign_in_params, only: [:create]
+  before_action :reject_customer, only: [:create]
+  
+  def after_sign_in_path_for(resource)
+    end_users_mypage_path
+  end
+  
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+  
+  def reject_customer
+    if current_end_user.is_deleted == true
+      reset_session
+      redirect_to root_path
+    end
+  end
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
+  
+  scope module: 'public' do
+    root :to => 'homes#top'
+    get 'about' => 'homes#about'
+    get 'end_users/mypage' => 'end_users#show', as: 'end_user'
+    get 'end_users/mypage/edit' => 'end_users#edit', as: 'edit_end_user'
+    patch 'end_users/mypage' => 'end_users#update'
+    get 'end_users/unsubscribe' => 'end_users#unsubscribe', as: 'unsubscribe'
+    patch 'end_users/withdraw' => 'end_users#withdraw', as: 'withdraw'
+    devise_for :end_users, controllers: {
+      sessions: 'public/end_users/sessions',
+      registrations: 'public/end_users/registrations'
+    }
+  end
+  
+  devise_for :admin, controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  namespace :admin do
+    get '/' => 'homes#top'
+  end
+  
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
